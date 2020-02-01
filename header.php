48f42633d3b28b8c6af54935bb41bc54e57f454d
helomdrs <heloisam.silva@hotmail.com>
2020-02-01 10:28:31 -0300

    header, footer e index

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- Fim do Boostrap -->
+  <link rel="stylesheet" href="css/header-style.css">
+  <link rel="stylesheet" href="css/index-style.css">
+
+
+  <title>Veterinária Pegadas</title>
+</head>
+<body>
+
+<!-- Menu de Navegação -->
+<header class="container">
+  <nav class="transparent navbar navbar-expand-lg navbar-light row d-flex justify-content-center">
+      <div class="navbar-logo">
+        <a class="navbar-brand" href="index.php">
+            <img src="images/Pegadas-logo.png" alt="Veterinária Pegadas Logo" class="logo-header">
+        </a>
+      </div>
+  </nav>
+  </header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require_once('header.php'); ?>
+
+<section id="bannerHome">
+
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block principalImage" src="images/Playful_cat.svg">
+                <div class="carousel-caption image-text col-lg-8 text-right d-none d-md-block">
+                    <button type="button" class="loginBtn btn btn-primary btn-lg">Cadastrar Cliente</button>
+                    <button type="button" class="loginBtn btn btn-primary btn-lg">Cadastrar Animal</button>
+                    <button type="button" class="loginBtn btn btn-primary btn-lg">Iniciar Atendimento</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php require_once('footer.php'); ?>
